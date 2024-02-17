5778e66b237be79757c0463928f6ffdbf4a013ea
akhramchikhina1 <159693504+akhramchikhina1@users.noreply.github.com>
2024-02-17 10:08:53 +0700

    13.py

--- 13.py
+++ 13.py
@@ -1,5 +1,5 @@
 import math
-range_radius=int(input("Введите значение радиуса "))
-blind_radius=int(input("Введите значение радиуса "))
+range_radius=int(input('Введите значение радиуса: '))
+blind_radius=int(input('Введите значение радиуса: '))
 area=abs(((range_radius **2) * math.pi)-((blind_radius **2) * math.pi))
-print("Площадь покрываемой территории:", area)
+print('Площадь покрываемой территории:', area)
