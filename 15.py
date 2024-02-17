7ae51fedf2ef27cb19235ab29f38089fbd33da85
akhramchikhina1 <159693504+akhramchikhina1@users.noreply.github.com>
2024-02-17 10:09:34 +0700

    15.py

--- 15.py
+++ 15.py
@@ -3,4 +3,4 @@ s_duim = s_sm/2.54
 s_fut= s_duim/12
 s_iard= s_fut/3
 s_milya= s_iard*1760
-print(s_iard, "ярдов\n", s_milya, "мили\n", s_fut, "футов\n", s_duim, "дюймов\n")
+print(s_iard, 'ярдов\n', s_milya, 'мили\n', s_fut, 'футов\n', s_duim, 'дюймов\n')
