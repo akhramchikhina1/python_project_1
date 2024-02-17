87073bcb75614e3b96c48fe33b88b7673a5967a8
akhramchikhina1 <a.khramchikhina1@g.nsu.ru>
2024-02-17 09:57:04 +0700

    print 13

--- 13.py
+++ 13.py
@@ -1,4 +1,5 @@
-operations=int((2*3-6)/3-4)
-number_1=int(input())
-number_2=float(number_1+operations)
-print(number_2)
+import math
+range_radius=int(input("Введите значение радиуса "))
+blind_radius=int(input("Введите значение радиуса "))
+area=abs(((range_radius **2) * math.pi)-((blind_radius **2) * math.pi))
+print("Площадь покрываемой территории:", area)
