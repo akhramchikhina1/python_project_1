f39a2bac25217d60a7340b5d86eeeff36e32b84e
akhramchikhina1 <159693504+akhramchikhina1@users.noreply.github.com>
2024-02-17 10:03:50 +0700

    7.py

--- 7.py
+++ 7.py
@@ -1,4 +1,4 @@
-name=str(input("Как Вас зовут? "))
-print("Здравствуйте,", name)
-hobby=str(input("Что Вам нравится? "))
-print("Отлично! ", hobby,"- хорошее увлечение")
+name=str(input('Как Вас зовут? '))
+print('Здравствуйте,', name)
+hobby=str(input('Что Вам нравится? '))
+print('Отлично! ', hobby,'- хорошее увлечение')
